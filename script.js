417145c43b4c355b6bd5b6e8219e44e081511088
iDharun7 <54797886+iDharun7@users.noreply.github.com>
2023-06-19 14:47:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function handleSubmit(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get values from the form
+  var name = document.getElementById('name').value;
+  var phoneNumber = document.getElementById('phoneNumber').value;
+  var email = document.getElementById('email').value;
+
+  // Redirect to the next page
+  window.location.href = 'mainpage.html?' + 'name=' + name + '&phoneNumber=' + phoneNumber + '&email=' + email;
+}
